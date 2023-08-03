566728a7e3a5b704b9b617b1be81dff0944a6cfc
Plinio Mabesi <pliniomabesi@gmail.com>
2023-08-02 21:44:14 -0300

    Marker icon

--- STEPS.md
+++ STEPS.md
@@ -7,6 +7,17 @@ npm install leaflet
 # Install React Leaflet
 npm install react-leaflet
 
+# Install Leaflet TS Types
+npm install -D @types/leaflet
+
+# Create and import map.css and leaflet.css
+.full-height-map {
+    height: 100vh;
+}
+
+import 'leaflet/dist/leaflet.css';
+import './map.css';
+
 # Import modules
 import { MapContainer, TileLayer, useMap } from 'react-leaflet'
 
@@ -15,11 +26,8 @@ import { MapContainer } from 'react-leaflet/MapContainer'
 import { TileLayer } from 'react-leaflet/TileLayer'
 import { useMap } from 'react-leaflet/hooks'
 
-# Install Leaflet TS Types
-npm install -D @types/leaflet
-
 # Basic Map
-<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+<MapContainer className='full-height-map' center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
   <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- src/MainMap.tsx
+++ src/MainMap.tsx
@@ -2,18 +2,27 @@ import React from 'react';
 import 'leaflet/dist/leaflet.css';
 import './map.css';
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import L, { LatLngTuple } from 'leaflet';
 
+const BAAN = [-16.242615990481234, -48.968309435936305] as LatLngTuple;
+const HOUSE = [-16.32249617862955, -48.92938431235188] as LatLngTuple;
+const MAP_MARKER = new L.Icon({
+  iconSize: [ 25, 32 ],
+  iconAnchor: [ 13, 41 ],
+  iconUrl: require('./marker.png')
+  //shadowUrl: require('./marker.svg')
+}) as L.Icon;
 
 function MainMap() {
   return (
-    <MapContainer className='full-height-map' center={[-16.32249617862955, -48.92938431235188]} zoom={10} scrollWheelZoom={true}>
+    <MapContainer className='full-height-map' center={HOUSE} zoom={14} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={BAAN} icon={MAP_MARKER} >
         <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
+          Base Aérea de Anápolis
         </Popup>
       </Marker>
     </MapContainer>
